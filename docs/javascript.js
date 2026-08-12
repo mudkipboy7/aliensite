ec8dfe6a9dd160b1636ad18a7d1b9b9771297b79
unknown <mudkipboy7@gmail.com>
2026-08-12 18:03:40 -0500

    Overhaul

---
+++ docs/javascript.js
@@ -0,0 +1,27 @@
+/*
+	Functions
+*/
+// Makes it go to the next line
+function ln(){
+	document.write("<br>");
+}
+// Draws a header
+function header(string,size,color){
+	document.write("<h1 style=font-size:" + size + ";color:" + color + ";>" + string + "</h1>");
+}
+
+// Draws the name of a site
+function siteName(string){
+	document.write("<v style=font-size:20px>" + string + "</v>");
+	ln();
+}
+
+// Draws a site link
+function siteLink(string, lnk){
+	document.write("<a style=font-size:20px href=" + lnk + ">" + string + "</a>");
+	ln();
+	ln();
+}
+
+
+
